b1bcbed56cb52b1331b06b35f91763066e7b0114
Svetlana Peredrii <sv_peredriy@mail.ru>
2016-01-16 21:06:22 +0200

    third commit

---
+++ Homework3/src/hw3/Guitar.java
@@ -0,0 +1,7 @@
+package hw3;
+
+/**
+ * Created by Влад on 16.01.2016.
+ */
+public class Guitar {
+}
---
+++ Homework3/src/hw3/Open_a_music_store.java
@@ -0,0 +1,7 @@
+package hw3;
+
+/**
+ * Created by Влад on 16.01.2016.
+ */
+public class Open_a_music_store {
+}
